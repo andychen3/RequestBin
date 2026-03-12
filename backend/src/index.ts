fbb5619d2c596e544e1b3effccda0d6d506a9b50
Danny C <35072063+dchen705@users.noreply.github.com>
2026-03-12 00:46:48 -0500

    feat: [backend] add live updates for bin requests via WebSocket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,49 @@
 
 import app from './app.js';
 import db from './services/db/index.js';
+import http from 'http';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const PORT = 3000;
 
 await db.init();
 
-const server = app.listen(PORT, () => {
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const subscriptions = new Map<string, Set<WebSocket>>();
+
+app.ws = {
+  server: wss,
+  subscriptions,
+}
+
+app.ws.server.on("connection", (ws) => {
+  console.log("client connected")
+
+  ws.on("message", (data) => {
+    const binRoute = data.toString()
+
+    const set = app.ws.subscriptions.get(binRoute) ?? new Set()
+
+    set.add(ws)
+    app.ws.subscriptions.set(binRoute, set)
+  })
+
+  ws.on("close", () => {
+    for (const set of app.ws.subscriptions.values()) {
+      set.delete(ws)
+    }
+    console.log('client disconnected');
+  })
+})
+
+server.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`)
-});
+})
 
 const shutdown = async (): Promise<void> => {
+  wss.close();
   server.close();
   await db.close();
   process.exit(0);
